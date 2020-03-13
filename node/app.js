4b14a8c0104d8194ee0a216b1fd7c73705ec6d7e
v_gwiguo <v_gwiguo@tencent.com>
2020-03-13 14:39:47 +0800

    更新抽奖名单

--- node/app.js
+++ node/app.js
@@ -1,8 +1,5 @@
-const express = require("express");
 const fs = require("fs");
 const readline = require("readline");
-const iconv = require('iconv-lite');
-
 fs.writeFileSync('./result.txt','');
 
 const rl = readline.createInterface({ 
@@ -23,27 +20,3 @@ rl.on('close',()=>{
     })
     fs.appendFileSync('./result.txt', JSON.stringify(data));
 })
-// var app = express();
-
-
-// app.use("*", function (req, res, next) {
-//     res.set({
-//         "Access-Control-Allow-Origin": "*"
-//     });
-//     next();
-// });
-
-// app.use("/", (req, res) => {
-//     fs.readFile('1.txt' ,(err, data) => {
-//         if (err) throw err;
-//         let member = [];
-//         data.toString().trim().replace(/\r/g, "").split("\n").forEach(item => {
-//             member.push({
-//                 "name" : item
-//             })
-//         })
-//         res.json(member);
-//     });
-// })
-
-// app.listen(3000);
